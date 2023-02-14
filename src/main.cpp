18c78ca6dd713db8c5cc244db48ba04f9768d694
mrblacknoir <thekaream123@gmail.com>
2023-02-14 21:53:26 +0000

    Added a loop to find the command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -5,4 +5,13 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    for (int i = 0; i < argc; i++ ){
+        cout << argv[i] << endl; 
+        if (std::string_view(argv[i]) == "--source"){
+            cout << "source found at index " << i << endl;
+        }
+        else if (std::string_view(argv[i]) == "--output"){
+            cout << " output found at index" << i << endl;
+        }
+    }
 }
