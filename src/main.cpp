a635a869487520af717c736b27e6b6fdb76f43a6
Owen Keary <38129415+MrGreyKnight@users.noreply.github.com>
2023-02-21 21:05:56 +0000

    Updated output messages & reformatted code

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,33 @@ int main(int argc, char **argv)
     string source = "";
     string output = "";
 
-    for (int i = 0; i < argc; i++ ){
-        if (std::string_view(argv[i]) == "--source"){
-            source = argv[i+1];
+    for (int i = 0; i < argc; i++)
+    {
+        if (std::string_view(argv[i]) == "--source")
+        {
+            source = argv[i + 1];
         }
-        else if (std::string_view(argv[i]) == "--output"){
-            output = argv[i+1];
+        else if (std::string_view(argv[i]) == "--output")
+        {
+            output = argv[i + 1];
         }
     }
-    if (source != "") {
-        cout << "This is the source..." << source << endl;
-    } else {
-        cout << "source is undefined" << endl;
-        return 0; 
+    if (source != "")
+    {
+        cout << "Source destination: " << source << endl;
     }
-    if (output != "") {
-        cout << "This is the output..." << output << endl;
-    } else {
-        cout << "output is undefined" << endl;
-        return 0; 
+    else
+    {
+        cout << "Source destination required. Please use the --source argument" << endl;
+        return 0;
+    }
+    if (output != "")
+    {
+        cout << "Output destination: " << output << endl;
+    }
+    else
+    {
+        cout << "Output destination required. Please use the --output argument" << endl;
+        return 0;
     }
 }
